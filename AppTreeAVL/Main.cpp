333b871240e8100ea01da20f603673417526b01f
Felipe Vignoli Matheiu <mathieufelipe@gmail.com>
2016-05-02 22:22:09 -0300

    Final code.

---
+++ AppTreeAVL/Main.cpp
@@ -0,0 +1,91 @@
+//Name: Felipe Vignoli Mathieu
+//Avl Tree.
+
+#include "Tree.h"
+
+void main()
+{
+	Tree *A = new Tree();
+
+	int i = 0;
+	int option = 0;
+	int e = 0, ex = 0;
+	char p;
+
+	do
+	{
+		cout << "Deseja efetuar qual operacao:" << endl;
+		cout << "1-> Inserir novo elemento;" << endl;
+		cout << "2-> Imprimir em PreOrder;" << endl;
+		cout << "3-> Imprimir em PostOrder;" << endl;
+		cout << "4-> Imprimir em InOrder;" << endl;
+		cout << "5-> Sair do programa;" << endl;
+		cout << "6-> Procurar elemento;" << endl;
+		cout << "7-> Display;" << endl;
+		cout << "8-> Remove elemento;" << endl;
+		cin >> option;
+		switch (option)
+		{
+		default:
+			break;
+		case 1:
+			cout << "Informe o elemento: ";
+			cin >> e;
+			A->insert(e);
+			break;
+		case 2:
+			A->printPreOrder(A->Root());
+			cout << endl;
+			break;
+		case 3:
+			A->printPostOrder(A->Root());
+			cout << endl;
+			break;
+		case 4:
+			A->printInOrder(A->Root());
+			cout << endl;
+			break;
+		case 5:
+			i = 1;
+			break;
+		case 6:
+			cout << endl;
+			cout << "Informe o elemento: ";
+			cin >> e;
+			ex = A->search(e, A->Root());
+			break;
+		case 7:
+			cout << endl;
+			A->display(A->Root(), 1);
+			cout << endl << endl << endl;
+			break;
+		case 8:
+			int x;
+			cout << "Informe o valor a ser removido: ";
+			cin >> x;
+			A->remove(A->Root(), x);
+		}
+		if (i != 1)
+		{
+			cout << "Deseja efetuar mais alguma operacao (S/N): ";
+			cin >> p;
+			switch (p)
+			{
+			default:
+				break;
+			case 's':
+				system("cls");
+				break;
+			case 'S':
+				system("cls");
+				break;
+			case 'N':
+				i = 1;
+				break;
+			case 'n':
+				i = 1;
+				break;
+			}
+		}
+	} while (i == 0);
+}
